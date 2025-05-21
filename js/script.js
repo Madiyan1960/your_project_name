3ddab4e0d03e87aef2a1002c56ac1f14e550c02b
Madiyan1960 <103851348+Madiyan1960@users.noreply.github.com>
2025-05-21 16:50:10 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const SUPABASE_URL = 'https://kpefeonxvgnfpgevkcwy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwZWZlb254dmduZnBnZXZrY3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzY4MDgsImV4cCI6MjA2MjgxMjgwOH0.aZJhwODNOS3FhyT8k-qAAfvo0NaYbv4QSm6SwuNaeys';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 // Получаем ссылки на элементы DOM
 const productsContainer = document.getElementById('products');
 const cartItemsContainer = document.getElementById('cartItems');
